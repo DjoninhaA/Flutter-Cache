fd88700685027a8e79a3f37131e904185112afb4
DjoninhaA <djonathan8016@gmail.comgit>
2025-02-24 13:08:05 -0300

    feat:criando classes e buildando apk

--- lib/contato.dart
+++ lib/contato.dart
@@ -3,5 +3,19 @@ class Contact {
   String name;
   String phone;
 
-  
+  Contact({this.id, required this.name, required this.phone});
+
+  factory Contact.fromMap(Map<String, dynamic> json) => Contact(
+        id: json['id'],
+        name: json['name'],
+        phone: json['phone'],
+      );
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'phone': phone,
+    };
+  }
 }
